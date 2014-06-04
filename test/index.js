aa11e1495217d0146f07c0bb2082b10a2c155dee
Dominic Tarr <dominic.tarr@gmail.com>
2014-06-04 22:18:41 +0000

    augment with fsms

--- test/index.js
+++ test/index.js
@@ -4,8 +4,13 @@ var mx = require('../')
 var pull = require('pull-stream')
 var assert = require('assert')
 
+var pullFSM = require('../fsm')
+
 var interleave = require('interleavings')
 
+function log (m) {
+  return pull.through(console.log.bind(null, m || ''))
+}
 
 interleave.test(function (async) {
 
@@ -19,21 +24,41 @@ interleave.test(function (async) {
       pull.through(function (data) {
         seen.push(data)
       }),
+    pullFSM(),
       async.through(),
+    pullFSM(),
       stream.sink
     )
   })
 
-  pull(A, async.through(), B, async.through(), A)
+  pull(A,     pullFSM(),
+async.through(),     
+log('>>'), B,     pullFSM(),
+log('<<'), async.through(), A)
 
   pull(
     pull.values([1,2,3]),
     async.through(),
+//    function (read) {
+//      var reading = false
+//      return function (abort, cb) {
+//        if(reading) throw new Error('already reading')
+//        reading = true
+//        read(abort, function (end, data) {
+//          reading = false
+//          console.log('READ', data, end)
+//          cb(end, data)
+//        })
+//      }
+//    },
+    pullFSM(),
     A.createStream(),
-    async.through(),
+//    async.through(),
+    pullFSM(),
     pull.collect(function (err, ary) {
       console.log(ary, '?')
       assert.deepEqual(ary, seen)
+      assert.deepEqual(ary, [1,2,3])
       async.done()
     })
   )

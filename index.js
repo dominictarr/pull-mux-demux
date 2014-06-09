4efd6a30d3871ec773a54f16656d548eed48b2bc
Dominic Tarr <dominic.tarr@gmail.com>
2014-06-09 13:21:20 +0000

    use new fork/switch api, where you can add a stream

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 //box the messages in a stream so that you can
 //track data/end and the id of that stream.
 
-var fork = require('pull-fork')
+var fork = require('pull-switch')
+//var fork = require('pull-fork')
 var many = require('pull-many')
 var pair = require('pull-pair')
 var pull = require('pull-stream')
@@ -11,7 +12,7 @@ var u = require('./util')
 
 var pullFSM = require('./fsm')
 
-module.exports = function (onConnection) {
+module.exports = function (onConnection, name) {
 
   var created = 0
   var received = 0
@@ -26,30 +27,26 @@ module.exports = function (onConnection) {
   var sources = many()
 
   function createPairs (id) {
-    console.log('createPairs', id)
     //this needs to be negative on responses
     //so we can handle streams from both sides.
+
     if(!streams[id]) {
       var p = pair()
       var q = pair()
       streams[id] = {
-        source: pull(p.source, pull.through(console.log)),
-        sink: pull(pull.through(console.log), q.sink)
+        source: p.source,
+        sink: q.sink
       }
       _streams[id] = {
         //this is the 'incoming' stream
         //it has a negative id. piped into the connection.
-        source: pull(q.source, pullFSM(), u.wrap(id)),
+        source: pull(q.source, u.wrap(id)),
         //this is the 'outgoing' stream,
         //it has a positive id.
-        sink:
-          pull(
-            u.unwrap(id*-1),
-            pull.through(console.log.bind(null, '?>')),
-            p.sink
-          )
+        sink: pull(u.unwrap(id*-1), p.sink)
       }
-    sources.add(_streams[id].source)
+
+      sources.add(_streams[id].source)
     }
   }
 
@@ -59,12 +56,13 @@ module.exports = function (onConnection) {
   }, function (id) {
 
     var has = !!streams[id]
-
+    //is this a response to my stream, or a new stream they created?
     createPairs(id)
 
-    if(!has) onConnection(streams[id])
+    if(id < 0)
+      onConnection(streams[id], id)
     return _streams[id].sink
-  })
+  }, null, name)
 
   return {
     //outer streams that are routed over network.
@@ -74,9 +72,8 @@ module.exports = function (onConnection) {
     //create a new dial-out stream.
     createStream: function () {
       var id = ++created
-      console.log('createStream', id)
       createPairs(id)
-      //forks is attached when it receives a message.
+      sinks.add(id, _streams[id].sink)
       return streams[id]
     }
   }
